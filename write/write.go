6e9996c2574afa815c48f3561f88ee83bc65f24e
Kyle Chan <kylec725@gmail.com>
2021-01-19 02:22:22 -0500

    add check for index bounds, also start pieceFiles function

--- write/write.go
+++ write/write.go
@@ -13,6 +13,7 @@ var (
     ErrFileExists = errors.New("Torrent's file already exists")
     ErrBadBlockBounds = errors.New("Received invalid bounds for a block")
     ErrCopyFailed = errors.New("Unexpected number of bytes copied")
+    ErrInvalidPiece = errors.New("Piece index was out of bounds")
 )
 
 // NewWrite sets up a new torrent file to write to
@@ -47,17 +48,31 @@ func pieceSize(to *torrent.Torrent, index int) int {
     return to.PieceLength
 }
 
+// pieceFiles returns the indexes of files the piece is a part of
+func pieceFiles(to *torrent.Torrent, index int) []int {
+    byteStart := index * to.PieceLength
+    byteEnd := byteStart + to.PieceLength
+    if byteEnd > to.TotalLength {
+        byteEnd = to.TotalLength
+    }
+    return nil
+}
+
 // AddBlock adds a block to a piece
 func AddBlock(to *torrent.Torrent, index, begin int, block, piece []byte) error {
+    if index < 0 || index >= to.TotalPieces {
+        return errors.Wrap(ErrInvalidPiece, "AddBlock")
+    }
+
     pieceSize := pieceSize(to, index)
-    end := begin + len(block) - 1 // last index in the block
+    end := begin + len(block) // last index + 1 in the block
 
     // Check if bounds are possible or if integer overflow has occurred
-    if begin < 0 || begin > (pieceSize - 1) || end < 0 || end > (pieceSize - 1) {
+    if begin < 0 || begin > (pieceSize - 1) || end - 1 < 0 || end > pieceSize {
         return errors.Wrap(ErrBadBlockBounds, "AddBlock")
     }
 
-    bytesCopied := copy(piece[begin:end+1], block)
+    bytesCopied := copy(piece[begin:end], block)
     if bytesCopied != len(block) {
         return errors.Wrap(ErrCopyFailed, "AddBlock")
     }
@@ -67,11 +82,19 @@ func AddBlock(to *torrent.Torrent, index, begin int, block, piece []byte) error 
 
 // AddPiece takes a torrent piece, and writes it to the appropriate file
 func AddPiece(to *torrent.Torrent, index int, piece []byte) error {
+    if index < 0 || index >= to.TotalPieces {
+        return errors.Wrap(ErrInvalidPiece, "AddPiece")
+    }
+
     return nil
 }
 
 // GetPiece returns a piece of a torrent as a byte slice
 func GetPiece(to *torrent.Torrent, index int) ([]byte, error) {
+    if index < 0 || index >= to.TotalPieces {
+        return nil, errors.Wrap(ErrInvalidPiece, "GetPiece")
+    }
+
     return nil, nil
 }
 
