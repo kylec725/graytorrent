232fc3bbe21523bb92450fce0e30862a5e6d1657
Kyle Chan <kylec725@gmail.com>
2021-01-23 01:18:52 -0500

    basic message retrieval functions

--- peer/message.go
+++ peer/message.go
@@ -5,6 +5,7 @@ import (
     "math"
     "fmt"
 
+    "github.com/kylec725/graytorrent/bitfield"
     "github.com/pkg/errors"
 )
 
@@ -29,33 +30,78 @@ var (
     ErrMessage = errors.New("Malformed message received")
 )
 
-// message stores the message type id and payload
-type message struct {
-    id messageID
-    payload []byte
+// Message stores the message type id and payload
+type Message struct {
+    ID messageID
+    Payload []byte
 }
 
-func (msg *message) encode() []byte {
+func (msg *Message) encode() []byte {
     if msg == nil {  // nil is a keep-alive message
         return make([]byte, 4)
     }
-    length := uint32(1 + len(msg.payload))
+    length := uint32(1 + len(msg.Payload))
     serial := make([]byte, length)
     binary.BigEndian.PutUint32(serial[0:4], length)
-    serial[4] = byte(msg.id)
-    copy(serial[5:], msg.payload)
+    serial[4] = byte(msg.ID)
+    copy(serial[5:], msg.Payload)
     return serial
 }
 
-func decode(data []byte) message {
+func decode(data []byte) Message {
     id := messageID(data[0])
     payload := data[1:]
-    return message{id, payload}
+    return Message{id, payload}
+}
+
+// Choke returns a choke message
+func Choke() Message {
+    return Message{ID: msgChoke}
+}
+
+// Unchoke returns an unchoke message
+func Unchoke() Message {
+    return Message{ID: msgUnchoke}
+}
+
+// Interested returns an interested message
+func Interested() Message {
+    return Message{ID: msgInterested}
+}
+
+// NotInterested returns a not interested message
+func NotInterested() Message {
+    return Message{ID: msgNotInterested}
+}
+
+// Have returns a have message
+// TODO
+func Have(index int) Message {
+    payload := make([]byte, 4)
+    binary.BigEndian.PutUint32(payload, uint32(index))
+    return Message{ID: msgNotInterested, Payload: payload}
+}
+
+// Bitfield returns a bitfield message
+// TODO
+func Bitfield(bf bitfield.Bitfield) Message {
+    return Message{}
+}
+
+// Request returns a request message for a piece
+// TODO
+func Request(index, begin, length int) Message {
+    return Message{}
+}
+
+// Piece returns a piece message containing a block
+// TODO
+func Piece(index, begin int, block []byte) Message {
+    return Message{}
 }
 
 // TODO replace with generic reading from connection
-// CURRENTLY UNUSED (REMOVE?)
-func (peer *Peer) rcvMessage() (*message, error) {
+func (peer *Peer) rcvMessage() (*Message, error) {
     buf := make([]byte, 4)
     if err := peer.Conn.Read(buf); err != nil {
         return nil, errors.Wrap(err, "rcvMessage")
@@ -79,17 +125,17 @@ func (peer *Peer) sendRequest(index, begin, length int) error {
     binary.BigEndian.PutUint32(payload[0:4], uint32(index))
     binary.BigEndian.PutUint32(payload[4:8], uint32(begin))
     binary.BigEndian.PutUint32(payload[8:12], uint32(length))
-    msg := message{id: msgRequest, payload: payload}
+    msg := Message{ID: msgRequest, Payload: payload}
 
     err := peer.Conn.Write(msg.encode())
     return errors.Wrap(err, "sendRequest")
 }
 
-func (peer *Peer) handleMessage(msg *message) error {
+func (peer *Peer) handleMessage(msg *Message) error {
     if msg == nil {
         // reset keep-alive
     }
-    switch msg.id {
+    switch msg.ID {
     case msgChoke:
         peer.peerChoking = true
     case msgUnchoke:
@@ -99,14 +145,14 @@ func (peer *Peer) handleMessage(msg *message) error {
     case msgNotInterested:
         peer.peerInterested = false
     case msgHave:
-        index := binary.BigEndian.Uint32(msg.payload)
+        index := binary.BigEndian.Uint32(msg.Payload)
         peer.Bitfield.Set(int(index))
     case msgBitfield:
         expected := int(math.Ceil(float64(peer.info.TotalPieces) / 8))
-        if expected != len(msg.payload) {
+        if expected != len(msg.Payload) {
             return errors.Wrap(ErrBitfield, "handleMessage")
         }
-        peer.Bitfield = msg.payload
+        peer.Bitfield = msg.Payload
     case msgRequest:
         if !peer.amChoking {
 
@@ -124,12 +170,12 @@ func (peer *Peer) handleMessage(msg *message) error {
 }
 
 // TODO
-func (peer *Peer) handleRequest(msg *message) error {
+func (peer *Peer) handleRequest(msg *Message) error {
     return nil
 }
 
 // TODO
-func (peer *Peer) handlePiece(msg *message) error {
+func (peer *Peer) handlePiece(msg *Message) error {
     // index := binary.BigEndian.Uint32(msg.payload[0:4])
     // begin := binary.BigEndian.Uint32(msg.payload[4:8])
     // block := msg.payload[8:]
