4c05d1bfbcca6b2f8482684cb57c2c6e58ebe05f
Kyle Chan <kylec725@gmail.com>
2021-02-02 23:04:33 -0500

    correct messages sent in unchoke alg

--- torrent/unchoke.go
+++ torrent/unchoke.go
@@ -3,27 +3,37 @@ package torrent
 import "github.com/kylec725/graytorrent/peer/message"
 
 func (to *Torrent) unchokeAlg() {
-	msgUnchoke := message.Unchoke()
-	// Choke all peers
-	for i := range to.Peers {
-		if !to.Peers[i].AmChoking {
-			to.Peers[i].SendMessage(msgUnchoke)
-		}
-	}
-	highRates := to.bestRates()
-	msgChoke := message.Choke()
 	// Unchoke the peers with the top 4 rates
+	msgUnchoke := message.Unchoke()
+	highRates := to.bestRates()
 	prevIndex := highRates[0]
 	for _, index := range highRates {
-		if index != prevIndex { // Make sure we don't unchoke the same peer again
-			to.Peers[index].SendMessage(msgChoke)
+		if to.Peers[index].AmChoking && index != prevIndex { // Make sure we don't unchoke the same peer again
+			to.Peers[index].SendMessage(msgUnchoke)
 		}
 		prevIndex = index
 	}
+
+	// Choke peers with lower rates
+	msgChoke := message.Choke()
+	for i := range to.Peers {
+		if !to.Peers[i].AmChoking && !numInSlice(i, highRates) {
+			to.Peers[i].SendMessage(msgChoke)
+		}
+	}
 }
 
-func (to *Torrent) bestRates() [4]int {
-	var highRates [4]int
+func numInSlice(index int, nums []int) bool {
+	for i := range nums {
+		if index == nums[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func (to *Torrent) bestRates() []int {
+	highRates := make([]int, 4)
 	// Find peers with top 4 download rates
 	for i, peer := range to.Peers {
 		if peer.Rate > to.Peers[highRates[0]].Rate {
