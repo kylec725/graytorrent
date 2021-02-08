46bb18d33b54ad472172e8981a64b2ba5b4997c0
Kyle Chan <kylec725@gmail.com>
2021-02-08 13:00:03 -0500

    move torrent saving to separate file

--- torrent/torrent.go
+++ torrent/torrent.go
@@ -135,15 +135,6 @@ func (to *Torrent) Start(ctx context.Context) {
 	}
 }
 
-// Save saves data about a managed torrent's state to a file
-func (to *Torrent) Save() {
-	// TODO log results of saving
-	// TODO consider have a directory, with a file for each torrent's state
-	// TODO alternative: open history file json maybe, see if we are in it, if not: add ourselves
-	//      if we are already, update info
-	return
-}
-
 func (to *Torrent) sendHave(index int) {
 	msg := message.Have(uint32(index))
 	for _, peer := range to.Peers {
