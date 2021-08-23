d7663bfe8e10352b75a7a4e6a49f2482733206d8
Kyle Chan <kylec725@gmail.com>
2021-08-23 15:04:50 -0400

    remove note about peer ids

--- peer/handshake/handshake.go
+++ peer/handshake/handshake.go
@@ -57,7 +57,7 @@ func Read(reader io.Reader) ([20]byte, error) {
 		return [20]byte{}, errors.Wrap(ErrPstrLen, "rcvHandshake")
 	}
 
-	buf = make([]byte, 48+pstrLen) // NOTE: some peers may not send a peerID
+	buf = make([]byte, 48+pstrLen)
 	if _, err := io.ReadFull(reader, buf); err != nil {
 		return [20]byte{}, errors.Wrap(err, "rcvHandshake")
 	}
