fafe6ef48a9c9f0eaa4feeb7a5adc881e8b52d8d
Kyle Chan <kylec725@gmail.com>
2021-09-13 01:59:22 -0400

    correct List function definition

--- server.go
+++ server.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
-
 	pb "github.com/kylec725/graytorrent/rpc"
 )
 
-func (s *torrentServer) List(ctx context.Context, in *pb.ListRequest) (pb.Torrent_ListClient, error) {
+func (s *torrentServer) List(in *pb.ListRequest, list pb.Torrent_ListServer) error {
 	server.Stop()
 	// return pb.Torrent_ListClient{}, nil
-	return nil, nil
+	return nil
 }
