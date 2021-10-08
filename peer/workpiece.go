aadc963d385d2ae9da0d94e33576a5ae6bd011f8
Kyle Chan <kylec725@gmail.com>
2021-10-07 23:10:28 -0400

    removed unneeded fields in workpiece

--- peer/workpiece.go
+++ peer/workpiece.go
@@ -1,30 +1,26 @@
 package peer
 
 import (
-	"time"
-
 	"github.com/kylec725/graytorrent/common"
 )
 
 type workPiece struct {
-	index     int
-	piece     []byte
-	left      int // bytes remaining in piece
-	curr      int // current byte position in slice
-	size      int // size of the piece
-	startTime time.Time
+	piece []byte
+	left  int // bytes remaining in piece
+	curr  int // current byte position in slice
+	size  int // size of the piece
 }
 
 func (p *Peer) addWorkPiece(info common.TorrentInfo, index int) {
 	pieceSize := common.PieceSize(info, index)
 	piece := make([]byte, pieceSize)
-	p.workPieces[index] = workPiece{index, piece, pieceSize, 0, pieceSize, time.Now()}
+	p.workPieces[index] = workPiece{piece, pieceSize, 0, pieceSize}
 }
 
 // clearWork sends peer's work back into the work pool
 func (p *Peer) clearWork(work chan int) {
-	for _, wp := range p.workPieces {
-		work <- wp.index
+	for index := range p.workPieces {
+		work <- index
 	}
 	p.workPieces = make(map[int]workPiece)
 }
