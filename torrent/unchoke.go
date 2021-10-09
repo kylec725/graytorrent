c18aee1efd70a0047cade06840d28b04fee7d8a5
Kyle Chan <kylec725@gmail.com>
2021-10-09 01:40:21 -0400

    change naming to accurately reflect best downloaders

--- torrent/unchoke.go
+++ torrent/unchoke.go
@@ -11,12 +11,12 @@ import (
 func (to *Torrent) unchokeAlg() {
 	msgUnchoke := message.Unchoke()
 	msgChoke := message.Choke()
-	highRates := to.highRatePeers()
+	bestDownloaders := to.bestDownloaders()
 
 	// Loop through all the peers, unchoke them if they are the optimistic unchoke or have a high rate of upload to us
 	for i := range to.Peers {
 		shouldChoke := true
-		for _, peerPtr := range highRates {
+		for _, peerPtr := range bestDownloaders {
 			if &to.Peers[i] == peerPtr && peerPtr.AmChoking { // Make sure we don't unchoke the same peer again
 				shouldChoke = false
 				to.Peers[i].AmChoking = false
@@ -38,27 +38,28 @@ func (to *Torrent) unchokeAlg() {
 	}
 }
 
-func (to *Torrent) highRatePeers() []*peer.Peer {
-	highRates := make([]*peer.Peer, 4)
+// bestDownloaders gets a list of the downloaders with the highest upload rate to us
+func (to *Torrent) bestDownloaders() []*peer.Peer {
+	bestDownloaders := make([]*peer.Peer, 4)
 	// Find peers with top 4 download rates
 	for i := range to.Peers {
-		if to.Peers[i].PeerInterested && (highRates[0] == nil || to.Peers[i].DownRate() > highRates[0].DownRate()) {
-			highRates[3] = highRates[2]
-			highRates[2] = highRates[1]
-			highRates[1] = highRates[0]
-			highRates[0] = &to.Peers[i]
-		} else if to.Peers[i].PeerInterested && (highRates[1] == nil || to.Peers[i].DownRate() > highRates[1].DownRate()) {
-			highRates[3] = highRates[2]
-			highRates[2] = highRates[1]
-			highRates[1] = &to.Peers[i]
-		} else if to.Peers[i].PeerInterested && (highRates[2] == nil || to.Peers[i].DownRate() > highRates[2].DownRate()) {
-			highRates[3] = highRates[1]
-			highRates[2] = &to.Peers[i]
-		} else if to.Peers[i].PeerInterested && (highRates[3] == nil || to.Peers[i].DownRate() > highRates[3].DownRate()) {
-			highRates[3] = &to.Peers[i]
+		if to.Peers[i].PeerInterested && (bestDownloaders[0] == nil || to.Peers[i].DownRate() > bestDownloaders[0].DownRate()) {
+			bestDownloaders[3] = bestDownloaders[2]
+			bestDownloaders[2] = bestDownloaders[1]
+			bestDownloaders[1] = bestDownloaders[0]
+			bestDownloaders[0] = &to.Peers[i]
+		} else if to.Peers[i].PeerInterested && (bestDownloaders[1] == nil || to.Peers[i].DownRate() > bestDownloaders[1].DownRate()) {
+			bestDownloaders[3] = bestDownloaders[2]
+			bestDownloaders[2] = bestDownloaders[1]
+			bestDownloaders[1] = &to.Peers[i]
+		} else if to.Peers[i].PeerInterested && (bestDownloaders[2] == nil || to.Peers[i].DownRate() > bestDownloaders[2].DownRate()) {
+			bestDownloaders[3] = bestDownloaders[1]
+			bestDownloaders[2] = &to.Peers[i]
+		} else if to.Peers[i].PeerInterested && (bestDownloaders[3] == nil || to.Peers[i].DownRate() > bestDownloaders[3].DownRate()) {
+			bestDownloaders[3] = &to.Peers[i]
 		}
 	}
-	return highRates
+	return bestDownloaders
 }
 
 func (to *Torrent) changeOptimisticUnchoke(lastOpUnchoke *time.Time) {
