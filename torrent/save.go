07a06df56f9cc137bcc855c781f630fad5eb0625
Kyle Chan <kylec725@gmail.com>
2021-10-10 11:42:38 -0400

    save individual files for managed torrents

--- torrent/save.go
+++ torrent/save.go
@@ -3,25 +3,29 @@ package torrent
 import (
 	"compress/gzip"
 	"encoding/json"
+	"io/fs"
 	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 
+	"github.com/kylec725/gray/internal/common"
 	"github.com/pkg/errors"
 )
 
-const saveDataType = ".gz"
+const saveType = ".gz"
 
-// SaveAll saves the states of all managed torrents
-func SaveAll(torrentList []Torrent) error {
-	jsonStream, err := json.Marshal(torrentList)
+// Save saves the progress of a torrent
+func (to *Torrent) Save() error {
+	// TODO: only save a file if the current last modified time matches our last modified time
+	jsonStream, err := json.Marshal(to)
 	if err != nil {
-		return errors.Wrap(err, "SaveAll")
+		return errors.Wrap(err, "Save")
 	}
 
-	file, err := os.Create(saveDataPath) // os.Create creates or truncates the named file
+	file, err := os.Create(path.Join(common.SavePath, to.Info.Name+saveType)) // os.Create creates or truncates the named file
 	if err != nil {
-		return errors.Wrap(err, "SaveAll")
+		return errors.Wrap(err, "Save")
 	}
 	defer file.Close()
 
@@ -29,40 +33,55 @@ func SaveAll(torrentList []Torrent) error {
 	writer := gzip.NewWriter(file)
 	defer writer.Close()
 	_, err = writer.Write(jsonStream)
-	if err != nil {
-		return errors.Wrap(err, "SaveAll")
-	}
+	return errors.Wrap(err, "Save")
+}
 
+// SaveAll saves the states of all managed torrents
+func (s *Session) SaveAll() error {
+	for _, to := range s.torrentList {
+		if err := to.Save(); err != nil {
+			return err // No need to wrap err
+		}
+	}
 	return nil
 }
 
 // LoadAll retrieves a list of all managed torrents
-func LoadAll() ([]Torrent, error) {
-	var torrentList []Torrent
+func LoadAll() (map[[20]byte]*Torrent, error) {
+	torrentList := make(map[[20]byte]*Torrent)
 
-	file, err := os.Open(saveDataPath)
-	if err != nil { // If the save data file doesn't exist, return an empty list
-		return torrentList, nil
-	}
-	defer file.Close()
+	err := filepath.WalkDir(common.SavePath, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() { // Ignore directories
+			return nil
+		}
+		file, err := os.Open(d.Name())
+		if err != nil { // If the save data file doesn't exist, return an empty list
+			return err
+		}
+		defer file.Close()
 
-	reader, err := gzip.NewReader(file)
-	if err != nil {
-		return nil, errors.Wrap(err, "LoadAll")
-	}
-	defer reader.Close()
+		reader, err := gzip.NewReader(file)
+		if err != nil {
+			return err
+		}
+		defer reader.Close()
 
-	fileBytes, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return nil, errors.Wrap(err, "LoadAll")
-	}
+		fileBytes, err := ioutil.ReadAll(reader)
+		if err != nil {
+			return err
+		}
 
-	err = json.Unmarshal(fileBytes, &torrentList)
-	if err != nil {
-		return nil, errors.Wrap(err, "LoadAll")
-	}
+		var to Torrent
+		err = json.Unmarshal(fileBytes, &to)
+		if err != nil {
+			return err
+		}
+		torrentList[to.InfoHash] = &to
 
-	return torrentList, nil
+		return nil
+	})
+
+	return torrentList, errors.Wrap(err, "LoadAll")
 }
 
 // TODO: verify existing file's present pieces
