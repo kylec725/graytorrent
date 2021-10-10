3a4c91e4caec00fd3197377b1ad1d4d1a047c957
Kyle Chan <kylec725@gmail.com>
2021-10-10 00:32:50 -0400

    moved catchInterrupt skeleton code here

--- torrent/session.go
+++ torrent/session.go
@@ -103,3 +103,22 @@ func (s *Session) Download(ctx context.Context, filename string) {
 	SaveAll(s.torrentList)
 	fmt.Println("Torrent done:", to.Info.Name)
 }
+
+// func catchInterrupt(ctx context.Context, cancel context.CancelFunc) {
+// 	signalChan := make(chan os.Signal, 1)
+// 	signal.Notify(signalChan, os.Interrupt)
+// 	select {
+// 	case <-signalChan: // Cleanup on interrupt signal
+// 		signal.Stop(signalChan)
+// 		peerListener.Close()
+// 		cancel()
+// 		err = torrent.SaveAll(torrentList)
+// 		if err != nil {
+// 			log.WithField("error", err).Debug("Problem occurred while saving torrent management data")
+// 		}
+// 		log.Info("Graytorrent stopped")
+// 		logFile.Close()
+// 		os.Exit(1)
+// 	case <-ctx.Done():
+// 	}
+// }
