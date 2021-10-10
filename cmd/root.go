83c86ed43aba084748f8fd6f8d86da595623d08d
Kyle Chan <kylec725@gmail.com>
2021-10-09 23:44:00 -0400

    moved init to root

--- cmd/root.go
+++ cmd/root.go
@@ -2,25 +2,105 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "graytorrent",
-	Short: "graytorrent is a BitTorrent engine",
-	Long:  `An engine that implements the BitTorrent Protocol and allows for the management of torrents.`,
-	// Run: func(cmd *cobra.Command, args []string) {
-	// 	// Do Stuff Here
-	// 	fmt.Println("hello root command")
-	// },
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "print events to stdout")
+	cobra.OnInitialize(initDirs, initLog, initConfig, viper.WatchConfig)
 }
+
+var (
+	logLevel        = log.TraceLevel // InfoLevel || DebugLevel || TraceLevel
+	logFile         *os.File
+	grayTorrentPath = filepath.Join(os.Getenv("HOME"), ".config", "graytorrent")
+
+	// Flags
+	verbose bool
+
+	rootCmd = &cobra.Command{
+		Use:   "graytorrent",
+		Short: "graytorrent is a BitTorrent engine",
+		Long:  `An engine that implements the BitTorrent Protocol and allows for the management of torrents.`,
+		// Run: func(cmd *cobra.Command, args []string) {
+		// 	// Do Stuff Here
+		// 	fmt.Println("hello root command")
+		// },
+	}
+)
 
 // Execute runs the root command
 func Execute() {
+	log.Info("Graytorrent started")
+	defer func() {
+		log.Info("Graytorrent stopped")
+		logFile.Close()
+	}()
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+}
+
+// initDirs initializes any necessary management directories
+func initDirs() {
+	err := os.MkdirAll(grayTorrentPath, os.ModePerm)
+	if err != nil {
+		log.WithField("error", err.Error()).Fatal("Could not create necessary directories")
+	}
+}
+
+func initLog() {
+	// Logging file
+	logFile, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	// logFile, err = os.OpenFile(filepath.Join(grayTorrentPath, "info.log"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.WithField("error", err.Error()).Fatal("Could not open log file")
+	}
+
+	// Set logging settings
+	log.SetOutput(logFile)
+	log.SetFormatter(&prefixed.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+		ForceFormatting: true,
+	})
+	if verbose {
+		dualOutput := io.MultiWriter(os.Stdout, logFile)
+		log.SetOutput(dualOutput)
+	}
+	log.SetLevel(logLevel)
+}
+
+func initConfig() {
+	viper.SetDefault("torrent.path", ".")
+	viper.SetDefault("torrent.autoseed", true)
+	viper.SetDefault("network.portrange", [2]int{6881, 6889})
+	viper.SetDefault("network.connections.globalMax", 300)
+	viper.SetDefault("network.connections.torrentMax", 30)
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	// viper.AddConfigPath(".") // Remove in the future
+	viper.AddConfigPath(grayTorrentPath)
+	viper.AddConfigPath("/etc/graytorrent")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found, create default config
+			viper.SafeWriteConfig()
+			log.Info("Config file written")
+		} else {
+			// Some other error was found
+			log.Panic("Fatal error reading config file:", err)
+		}
 	}
 }
--- main.go
+++ main.go
@@ -5,56 +5,29 @@ import (
 	"path/filepath"
 
 	"github.com/kylec725/graytorrent/cmd"
-	pb "github.com/kylec725/graytorrent/rpc"
 	log "github.com/sirupsen/logrus"
-	flag "github.com/spf13/pflag"
-	viper "github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
 
 const (
-	logLevel   = log.TraceLevel // InfoLevel || DebugLevel || TraceLevel
-	serverPort = ":7001"        // GRPC server port
+	serverPort = ":7001" // GRPC server port
 )
 
 var (
-	err     error
-	logFile *os.File
-
-	// Flags
-	verbose bool
+	err error
 
 	grayTorrentPath = filepath.Join(os.Getenv("HOME"), ".config", "graytorrent")
 	server          *grpc.Server
 )
 
-type torrentServer struct {
-	pb.UnimplementedTorrentServer
-}
-
-func init() {
-	flag.BoolVarP(&verbose, "verbose", "v", false, "print events to stdout")
-	flag.Parse()
-}
-
 func main() {
-	err = os.MkdirAll(grayTorrentPath, os.ModePerm)
-	if err != nil {
-		log.WithField("error", err.Error()).Fatal("Could not create necessary directories")
-	}
-
-	initLog()
-	log.Info("Graytorrent started")
-
-	initConfig()
-	viper.WatchConfig()
 
 	// setupListen()
 
 	// Cleanup
 	defer func() {
 		log.Info("Graytorrent stopped")
-		logFile.Close()
+		// logFile.Close()
 	}()
 
 	cmd.Execute()
