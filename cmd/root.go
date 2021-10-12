0ade5a2f856666975b3b91cebb187c498d95b770
Kyle Chan <kylec725@gmail.com>
2021-10-12 14:20:37 -0400

    changed order of init functions

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "print events to stdout")
-	cobra.OnInitialize(initConfig, initDirs)
+	cobra.OnInitialize(initDirs, initConfig)
 }
 
 var (
