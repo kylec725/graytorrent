7afc6f3ee8eab450b1fdb01b9fd1519916d5dc9f
Kyle Chan <kylec725@gmail.com>
2021-10-13 19:37:43 -0400

    unignored necessary file for binary

---
+++ cmd/gray/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/kylec725/graytorrent/cmd"
+)
+
+// Launches cobra, code execution begins in cmd/root.go
+func main() {
+	cmd.Execute()
+
+	// Setup grpc server
+	// TODO: Want to use TLS for encrypting communication
+	// serverListener, err := net.Listen("tcp", serverPort)
+	// if err != nil {
+	// 	log.WithFields(log.Fields{"error": err.Error(), "port": serverPort[1:]}).Fatal("Failed to listen for rpc")
+	// }
+	// server = grpc.NewServer()
+	// pb.RegisterTorrentServer(server, &torrentServer{})
+	// if err = server.Serve(serverListener); err != nil {
+	// 	log.WithField("error", err).Debug("Issue with serving rpc client")
+	// }
+}
