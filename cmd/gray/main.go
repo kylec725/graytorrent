a50415a082d4185614af532cc4a4eeafe6a76649
Kyle Chan <kylec725@gmail.com>
2021-10-14 21:36:36 -0400

    cleanup

--- cmd/gray/main.go
+++ cmd/gray/main.go
@@ -7,16 +7,4 @@ import (
 // Launches cobra, code execution begins in cmd/root.go
 func main() {
 	cmd.Execute()
-
-	// Setup grpc server
-	// TODO: Want to use TLS for encrypting communication
-	// serverListener, err := net.Listen("tcp", serverPort)
-	// if err != nil {
-	// 	log.WithFields(log.Fields{"error": err.Error(), "port": serverPort[1:]}).Fatal("Failed to listen for rpc")
-	// }
-	// server = grpc.NewServer()
-	// pb.RegisterTorrentServer(server, &torrentServer{})
-	// if err = server.Serve(serverListener); err != nil {
-	// 	log.WithField("error", err).Debug("Issue with serving rpc client")
-	// }
 }
